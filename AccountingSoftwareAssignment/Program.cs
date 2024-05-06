6b8ed92207f419b9a24af3a88fc0ac0052126e4d
Zsaigal <139230936+Zsaigal@users.noreply.github.com>
2024-05-06 10:19:25 +0500

    Add files via upload

---
+++ AccountingSoftwareAssignment/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to the Acme Accounting Systems");
+        Console.ReadLine();
+        Console.WriteLine("Remember, we're \"accounting\" on you!");
+        Console.ReadLine();
+
+    }
+}
+
